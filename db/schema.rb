0c41b08308d1cafc7abbe912453610147a5a3012
aleksacastle <aaklochko@gmail.com>
2018-08-12 03:13:10 +0300

    Added basic mvc for recipe

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171219160615) do
+ActiveRecord::Schema.define(version: 20180812002528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recipes", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "todos", force: :cascade do |t|
     t.string   "name"
